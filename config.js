0698f6ee8c57de2c6da3cb693cc18d990e26e35b
Pakitow <benjamin@pakitow.eu>
2016-02-04 14:03:50 +0100

    Conf

---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {};
+
+// Global configuration
+config.port = 1337;
+
+module.exports = config;
--- server.js
+++ server.js
@@ -3,3 +3,22 @@ var express			= require('express'),
 	config 			= require('./config'),
 	app 			= express();
 
+//remove powered by and etag
+app.disable('x-powered-by');
+app.disable('etag');
+
+//middleware
+app.use(bodyParser.urlencoded({extended: false}))	
+   .use(bodyParser.json());
+
+//catch all other request
+app.use(function(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(404).send('Not Found');
+});
+
+//launch server
+var server = app.listen(config.server.port, function () {
+  var port = server.address().port;
+  console.log('API rocking on port %s', port);
+});
